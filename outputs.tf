34443e6cba771709ca8be3bc080950fc3291ea0b
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-08-26 16:36:42 +0530

    peering

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,9 @@ output "database_subnet_ids" {
 output "database_subnet_group_id" {
     value = aws_db_subnet_group.default.id
 }
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
+output "igw" {
+    value = aws_internet_gateway.gw.id
+}
