dadf7f8c477ee6f9eedf6a90779796018d72b487
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-01-17 23:06:44 +0530

    adding default vpc route table for vpc peering

--- peering.tf
+++ peering.tf
@@ -41,7 +41,7 @@ resource "aws_route" "default_peering" {
   count = var.is_peering_required && var.acceptor_vpc_id == "" ? 1 : 0
   # main route table is created for default vpc
   # thus we are adding default vpc main route table to the Acceptor-VPC by peering connection
-  route_table_id            = data.aws_route_table.main.id # selecting default vpc main route table 
-  destination_cidr_block    = var.vpc_cidr # selecting expense vpc CIDR
+  route_table_id            = data.aws_route_table.main.id # selecting default vpc main route table by using data source 
+  destination_cidr_block    = var.vpc_cidr # selecting expense vpc CIDR 
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id # peering[0] must be included because we are using count and count is a list
 }
